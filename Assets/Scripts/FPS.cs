a6fe1652c83c626aeef72b75d71432aa48e37e14
DESKTOP-DA7P7LE\isara <sarath4@icloud.com>
2018-04-02 11:33:02 +0530

    Inital Commit

---
+++ Assets/Scripts/FPS.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPS : MonoBehaviour {
+
+	// Use this for initialization
+	    public TextMesh fpsText;
+     public float deltaTime;
+	 void Awake()
+	 {
+		  Application.targetFrameRate = 300;
+	 }
+ 
+     void Update () {
+         deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+         float fps = 1.0f / deltaTime;
+         fpsText.text = Mathf.Ceil (fps).ToString ();
+     }
+}
---
+++ Assets/Scripts/RecieveCommands.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecieveCommands : MonoBehaviour {
+
+	// Use this for initialization
+    public Transform target;
+    public float speed;
+	public void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+        //     float step = speed * Time.deltaTime;
+        // transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
